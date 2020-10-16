a86f10e0e09482307332dc4ad67f2d32f04f1e80
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-16 12:50:59 +0100

    Build: found id's from sign up xml page

--- app/src/main/java/com/example/project_chow/SignUp.java
+++ app/src/main/java/com/example/project_chow/SignUp.java
@@ -3,12 +3,32 @@ package com.example.project_chow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
+
+    MaterialEditText edtPhone, edtName, edtPassword;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        edtName = (MaterialEditText)findViewById(R.id.edtName);
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+
+
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+
     }
 }
