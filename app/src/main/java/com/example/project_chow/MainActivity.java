785461e1bd28d9e66d9ffe32216d0fc29dadc8c4
joshSteadman <josh.steadman15@gmail.com>
2020-10-16 11:58:48 +0100

    Style: WIP adding font

--- app/src/main/java/com/example/project_chow/MainActivity.java
+++ app/src/main/java/com/example/project_chow/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.project_chow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
     }
 }
