3e6b5dc20208b35f37b2b07decd374224b4716db
ZishJawaid <zisheanj@gmail.com>
2020-10-18 12:40:16 +0100

    Build: WIP link food detail to firebase

--- app/src/main/java/com/example/project_chow/FoodDetail.java
+++ app/src/main/java/com/example/project_chow/FoodDetail.java
@@ -3,12 +3,58 @@ package com.example.project_chow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FoodDetail extends AppCompatActivity {
+
+    TextView food_name, food_price, food_description;
+    ImageView food_image;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    FloatingActionButton btnCart;
+    ElegantNumberButton numberButton;
+
+    String foodId="";
+
+    FirebaseDatabase database;
+    DatabaseReference foods;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        foods = database.getReference("Food");
+
+        //Init view
+        numberButton = (ElegantNumberButton)findViewById(R.id.number_button);
+        btnCart = (FloatingActionButton)findViewById(R.id.btnCart);
+
+        food_description = (TextView)findViewById(R.id.food_description);
+        food_name = (TextView)findViewById(R.id.food_name);
+        food_price = (TextView)findViewById(R.id.food_price);
+        food_image = (ImageView) findViewById(R.id.img_food);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppbar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppbar);
+
+        //Get Food ID from Intent
+        if(getIntent() != null)
+            foodId = getIntent().getStringExtra("FoodId");
+        if(!foodId.isEmpty())
+        {
+            getDetailFood(foodId);
+        }
+
     }
-}
