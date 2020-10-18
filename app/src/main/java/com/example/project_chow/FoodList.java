ce316547f0347b3e2e4fdfde5a8e50363e9f9d58
ZishJawaid <zisheanj@gmail.com>
2020-10-18 11:53:40 +0100

    Build: Connect menu list to firebase

--- app/src/main/java/com/example/project_chow/FoodList.java
+++ app/src/main/java/com/example/project_chow/FoodList.java
@@ -1,14 +1,36 @@
 package com.example.project_chow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class FoodList extends AppCompatActivity {
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class FoodList extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    FirebaseDatabase database;
+    DatabaseReference foodList;
+    String categoryId = "";
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
+        database = FirebaseDatabase.getInstance();
+        foodList = database.getReference("Food");
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_food);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        if(getIntent() != null)
+            categoryId = getIntent().getStringExtra("CategoryId");
+        if(!categoryId.isEmpty() && categoryId != null)
+        {
+            loadListFood(categoryId);
+        }
     }
-}
