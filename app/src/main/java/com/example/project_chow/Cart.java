7bf4e9897ae4ea26afba0ad96d42215e16c1134c
Djura22 <michaeliangroves@gmail.com>
2020-10-18 11:50:24 +0100

    Build: Cart integration with Firebase, Adapter + REQ class created

--- app/src/main/java/com/example/project_chow/Cart.java
+++ app/src/main/java/com/example/project_chow/Cart.java
@@ -1,14 +1,70 @@
 package com.example.project_chow;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.project_chow.Database.Database;
+import com.example.project_chow.ViewHolder.CartAdapter;
+import com.example.project_chow.Common.Common;
+import com.example.project_chow.Model.Order;
+import com.example.project_chow.Model.Request;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import info.hoang8f.widget.FButton;
 
 public class Cart extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference requests;
+
+    TextView txtTotalPrice;
+    FButton btnPlace;
+
+    List<Order> cart = new ArrayList<>();
+
+    CartAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        database = FirebaseDatabase.getInstance();
+        requests = database.getReference("Requests");
+
+        recyclerView = (RecyclerView)findViewById(R.id.listCart);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        txtTotalPrice = (TextView)findViewById(R.id.total);
+        btnPlace = (FButton)findViewById(R.id.btnPlaceOrder);
+
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
+
+        loadListFood();
     }
-}
---
+++ app/src/main/java/com/example/project_chow/ViewHolder/CartAdapter.java
@@ -0,0 +1,4 @@
+package com.example.project_chow.ViewHolder;
+
+public class CartAdapter {
+}
