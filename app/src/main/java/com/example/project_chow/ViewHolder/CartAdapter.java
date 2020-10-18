a821b95e11534e652a49cf541521a302deacfcce
JRG <47917431+JR-G@users.noreply.github.com>
2020-10-18 12:00:51 +0100

    Build: Cart adapter

--- app/src/main/java/com/example/project_chow/ViewHolder/CartAdapter.java
+++ app/src/main/java/com/example/project_chow/ViewHolder/CartAdapter.java
@@ -1,5 +1,6 @@
 package com.example.project_chow.ViewHolder;
 
+
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
@@ -44,5 +45,43 @@ class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickList
     @Override
     public void onClick(View v) {
 
+    }
+}
+
+public class CartAdapter extends RecyclerView.Adapter<CartViewHolder> {
+
+    private List<Order> listData = new ArrayList<>();
+    private Context context;
+
+    public CartAdapter(List<Order> listData, Context context) {
+        this.listData = listData;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View itemView = inflater.inflate(R.layout.cart_layout, parent, false);
+        return new CartViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound(""+listData.get(position).getQuantity(), Color.RED);
+        holder.img_cart_count.setImageDrawable(drawable);
+
+        Locale locale = new Locale("en", "US");
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+        int price = (Integer.parseInt(listData.get(position).getPrice()))*(Integer.parseInt(listData.get(position).getQuantity()));
+        holder.txt_price.setText(fmt.format(price));
+        holder.txt_cart_name.setText(listData.get(position).getProductName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
     }
 }
