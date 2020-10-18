c10dc040239c24483313346985d3aa7f0b861b44
Djura22 <michaeliangroves@gmail.com>
2020-10-18 11:55:38 +0100

    Build: CartAdapter constructed, layout filled

--- app/src/main/java/com/example/project_chow/ViewHolder/CartAdapter.java
+++ app/src/main/java/com/example/project_chow/ViewHolder/CartAdapter.java
@@ -1,4 +1,48 @@
 package com.example.project_chow.ViewHolder;
 
-public class CartAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.example.project_chow.Interface.ItemClickListener;
+import com.example.project_chow.R;
+import com.example.project_chow.Model.Order;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+    public TextView txt_cart_name, txt_price;
+    public ImageView img_cart_count;
+
+    private ItemClickListener itemClickListener;
+
+    public void setTxt_cart_name(TextView txt_cart_name) {
+        this.txt_cart_name = txt_cart_name;
+    }
+
+    public CartViewHolder(@NonNull View itemView) {
+        super(itemView);
+        txt_cart_name = (TextView)itemView.findViewById(R.id.cart_item_name);
+        txt_price = (TextView)itemView.findViewById(R.id.cart_item_Price);
+        img_cart_count = (ImageView)itemView.findViewById(R.id.cart_item_count);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
