40f4a19ff25e6def9724cfdfc7d58ea19999ff23
Djura22 <michaeliangroves@gmail.com>
2020-10-18 12:08:41 +0100

    Build: Cart buttons and flash messages implemented

--- app/src/main/java/com/example/project_chow/Cart.java
+++ app/src/main/java/com/example/project_chow/Cart.java
@@ -68,3 +68,66 @@ public class Cart extends AppCompatActivity {
 
         loadListFood();
     }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("One more step!");
+        alertDialog.setMessage("Enter your address: ");
+
+        final EditText editAddress = new EditText(Cart.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        editAddress.setLayoutParams(lp);
+        alertDialog.setView(editAddress);
+        alertDialog.setIcon(R.drawable.ic_baseline_shopping_cart_24);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Request request = new Request(
+                        Common.currentUser.getPhone(),
+                        Common.currentUser.getName(),
+                        editAddress.getText().toString(),
+                        txtTotalPrice.getText().toString(),
+                        cart
+                );
+
+                //Submit to Firebase
+                requests.child(String.valueOf(System.currentTimeMillis()))
+                        .setValue(request);
+
+                new Database(getBaseContext()).cleanCart();
+                Toast.makeText(Cart.this, "Thank you, Order Placed", Toast.LENGTH_SHORT).show();
+                finish();
+
+
+            }
+        });
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    private void loadListFood() {
+        cart = new Database(this).getCarts();
+        adapter = new CartAdapter(cart, this);
+        recyclerView.setAdapter(adapter);
+
+        int total = 0;
+        for(Order order:cart)
+            total+=(Integer.parseInt(order.getPrice()))*(Integer.parseInt(order.getQuantity()));
+        Locale locale = new Locale("en", "US");
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+
+        txtTotalPrice.setText(fmt.format(total));
+    }
+}
