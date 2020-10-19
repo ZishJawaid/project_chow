b2353f77565c9a7ef691c68380c37865d160976b
ZishJawaid <zisheanj@gmail.com>
2020-10-19 14:19:42 +0100

    Build: create chef class

--- app/src/main/java/com/example/project_chow/Model/Chef.java
+++ app/src/main/java/com/example/project_chow/Model/Chef.java
@@ -1,4 +1,40 @@
 package com.example.project_chow.Model;
 
+package com.example.project_chow.Model;
+
+import android.media.Image;
 public class Chef {
+    private String Name, Image, Description, MenuId;
+    public Chef() {
+    }
+    public Chef(String name, String image, String description, String menuId) {
+        Name = name;
+        Image = image;
+        Description = description;
+        MenuId = menuId;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getImage() {
+        return Image;
+    }
+    public void setImage(String image) {
+        Image = image;
+    }
+    public String getDescription() {
+        return Description;
+    }
+    public void setDescription(String description) {
+        Description = description;
+    }
+    public String getMenuId() {
+        return MenuId;
+    }
+    public void setMenuId(String menuId) {
+        MenuId = menuId;
+    }
 }
